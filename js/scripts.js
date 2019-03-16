12d5adc3078f026f63dddf26c134526e168a3f60
Kyle Boling <kmboling@gmail.com>
2019-03-15 23:02:07 -0400

    add first new input

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,20 @@
 let newTask = document.querySelector('input');
-// let list = document.querySelector('ul');
-// let	taskValue = newTask.value;
+let list = document.querySelector('ul');
+let	taskValue = newTask.value;
 
 
 function addTask(event) {
-	let listItem;
+	let listItem, listItemText;
 	if (event.keyCode === 13) {
 		if (newTask.value === "") {
 			return;
 		}
 		console.log('added');
-		// listItem = document.createElement('li'); //create new li
-		// listItem.textContent = taskValue; //add the user's input value
-		// list.appendChild('listItem'); //append new li to the ul
-		// taskValue = '';
+		listItem = document.createElement('li'); //create new li
+		listItemText = document.createTextNode(taskValue); //create new text node from input
+		listItem.appendChild(listItemText); //append user's input value to li
+		list.appendChild(listItem); //append new li to the ul
+		taskValue = "";
 	}
 };
 
